75a979a56acb901c597b463cb705e03498e760ff
rafihabibi <rafi90974@gmail.com>
2026-03-29 17:08:02 +0700

    refactor:update variable names for better readability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html", "./**/*.js"], // Ini wajib ada supaya Tailwind membaca file HTML di foldermu
+  content: ["./**/*.html", "./**/*.js"],
   theme: {
     extend: {
       colors: {
-        'dark-bg': '#0B1120',    /*  warna latar belakang */
-        'dark-card': '#1E293B',  /* kode Hex untuk kotak form (sedikit lebih terang dari background) */
-        'primary': '#CE952A',    /* Ganti dengan kode Hex warna aksen untuk tombol/logo */
+        'dark-bg': '#0B1120', 
+        'dark-card': '#1E293B',
+        'primary': '#CE952A', 
       },
       fontFamily: {
         'style': ['Montserrat', 'sans-serif'], 
